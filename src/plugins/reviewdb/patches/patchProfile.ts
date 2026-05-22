901cb07c6fec9985de6d3c2d4f9b0198106871e2
VenusIsJaded <VenusIsJaded@gmail.com>
2026-05-22 16:40:38 -0500

    fix(reviewdb): re-apply undefined guard to patchProfile.ts [skip ci]

--- src/plugins/reviewdb/patches/patchProfile.ts
+++ src/plugins/reviewdb/patches/patchProfile.ts
@@ -10,21 +10,25 @@ if (UserProfile === undefined)
     UserProfile = findByTypeName("UserProfileContent");
 
 export default () =>
-    after("type", UserProfile, (args, ret) => {
-        const profileSections = findInReactTree(
-            ret,
-            r =>
-                r?.type?.displayName === "View" &&
-                // UserProfileBio still exists even when the user has no bio. Yep.
-                r?.props?.children.findIndex(
-                    (i: any) =>
-                        i?.type?.name === "UserProfileBio" ||
-                        i?.type?.name === "UserProfileAboutMeCard",
-                ) !== -1,
-        )?.props?.children;
+    UserProfile !== undefined
+        ? after("type", UserProfile, (args, ret) => {
+            const profileSections = findInReactTree(
+                ret,
+                r =>
+                    r?.type?.displayName === "View" &&
+                    // UserProfileBio still exists even when the user has no bio. Yep.
+                    r?.props?.children.findIndex(
+                        (i: any) =>
+                            i?.type?.name === "UserProfileBio" ||
+                            i?.type?.name === "UserProfileAboutMeCard",
+                    ) !== -1,
+            )?.props?.children;
 
-        let userId = args[0]?.userId;
-        if (userId === undefined) userId = args[0]?.user?.id;
+            let userId = args[0]?.userId;
+            if (userId === undefined) userId = args[0]?.user?.id;
 
-        profileSections?.push(React.createElement(ReviewSection, { userId }));
-    });
+            profileSections?.push(React.createElement(ReviewSection, { userId }));
+        })
+        : (): boolean => {
+            return false;
+        };
